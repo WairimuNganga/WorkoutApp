1d4b4ecfd7679d37f64f862d71d7a646241fb164
WairimuNganga <wairimuznganga@gmail.com>
2022-06-18 09:03:01 +0300

    Activated the plan fragment and profile fragment

--- app/src/main/java/dev/wairimu/workoutlog/HomeActivity.kt
+++ app/src/main/java/dev/wairimu/workoutlog/HomeActivity.kt
@@ -3,10 +3,48 @@ package dev.wairimu.workoutlog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentContainerView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeActivity : AppCompatActivity() {
+    lateinit var fcvHome:FragmentContainerView
+    lateinit var  bnvHome:BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        castView()
+        setUpBottomNav()
+
+    }
+    fun castView(){
+        fcvHome = findViewById(R.id.fcvHome)
+        bnvHome = findViewById(R.id.bnvHome)
+    }
+    fun setUpBottomNav(){
+//        This will help switch to a different navigation
+        bnvHome.setOnItemSelectedListener { item->
+            when(item.itemId) {
+                R.id.plan -> {
+//                    val transaction = supportFragmentManager.beginTransaction()
+//                    transaction.replace(R.id.fcvHome, PlanFragment())
+//                    transaction.commit()
+                    supportFragmentManager.beginTransaction().replace(R.id.fcvHome, PlanFragment()).commit()
+                    true
+                }
+
+
+                R.id.track -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.fcvHome, TrackFragment()).commit()
+                    true
+                }
+
+                R.id.profile -> {
+                  supportFragmentManager.beginTransaction().replace(R.id.fcvHome, ProfileFragment()).commit()
+                    true
+                }
+                else->false
+            }
+        }
     }
 }
